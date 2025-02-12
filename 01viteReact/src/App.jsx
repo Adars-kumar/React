189f4d6f20ba0f87e00118883d4183052f49ab34
Adarsh Kumar <adarshkr.227@gmail.com>
2025-02-12 17:37:25 +0530

    Creating a custom react app and learning how does react+vite app work in main.jsx

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = 'chai aur code'
   return (
     <>
-      <h1>Chai in React</h1>
+      <h1>Chai in React | {username}</h1>
       <Chai />
     </>
   )
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,9 +1,35 @@
-
+// import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+
+
+
+// function myApp(){
+//     return(
+//         <div>
+//             <h1>Custom App !</h1>
+//         </div>
+//     )
+// }
+
+// const anotherElement = (
+//     <a href='https://google.com' target='_blank'>Visit Google</a>
+// )
+
+// const anotherUser = "Chai aur React"
+// const reactElemet = React.createElement(
+//     'a',
+//     {href: 'https://google.com', target: '_blank'},
+//     'Click me to visit google',
+//      anotherUser
+// )
+
 
 createRoot(document.getElementById('root')).render(
   
     <App />
+    // myApp()
+    // anotherElement
+    // reactElemet
   
 )
